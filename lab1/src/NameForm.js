b4a837b9ecbe0a0b6e31dae86fec44430b7b5480
Christina <vines@knights.ucf.edu>
2019-02-21 00:29:08 -0500

    lab1

--- lab1/src/NameForm.js
+++ lab1/src/NameForm.js
@@ -2,7 +2,9 @@ import React from 'react';
 class NameForm extends React.Component {
       constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {value: '',
+                      invalid:true,
+                       valid:false};
     
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -12,29 +14,35 @@ class NameForm extends React.Component {
         this.setState({value: event.target.value});
       }
     
+        
+    
       handleSubmit(event) {
-        let Name = this.state.value;
-        let alphabet = /^[A-Za-z]+$/;
-          if(Name.match(alphabet)) {
-              document.write("Hello User! Your Name is ",":" +" "+ Name);
-              return true;
-          } else{
-              alert("Error the User Used Non-Ascii Characters!");
-              return false;
+        if(/[^a-zA-Z]/.test(this.state.value)){
+            this.setState({invalid: false});
+          }else {
+            this.setState({invalid: true});
+            this.setState({valid: true});
+
           }
         event.preventDefault();
       }
     
       render() {
         return (
+            <div>
+            {(!this.state.valid)?
           <form onSubmit={this.handleSubmit}>
             <label>
               Name:
               <input type="text" value={this.state.value} onChange={this.handleChange} />
             </label>
             <input type="submit" value="Submit" />
-          </form>
-        );
+            {(!this.state.invalid)  ? <div style ={{color: "red"}}>Error the User Used Non-Ascii Characters!</div> : null}
+          </form>  :
+          <span> Hello User! Your Name is {this.state.value}.</span>
+          }
+        </div>
+        );      
       }
     }
  export default NameForm;
