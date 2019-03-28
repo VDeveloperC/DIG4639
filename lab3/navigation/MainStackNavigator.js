f92acd9ee890bf4359b2ff735d2ceae855aa6748
Christina <vines@knights.ucf.edu>
2019-03-27 23:00:02 -0400

    lab3

---
+++ lab3/navigation/MainStackNavigator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import HomeScreen from '../screens/HomeScreen';
+import Detail from '../screens/Detail';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+  Detail: Detail
+});
+
+export default HomeStack;
+
